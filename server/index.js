ca26acf044aa0d03d0768ed2460fc18da7070165
Ruveyda <ruveyday20@gmail.com>
2022-10-15 22:40:36 +0300

    feat: 🎸 add sockets

---
+++ server/index.js
@@ -0,0 +1,92 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import "./addRequire.js";
+
+const io = require("socket.io")(8800, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+  // add new User
+  socket.on("new-user-add", (newUserId) => {
+    // if user is not added previously
+    if (!activeUsers.some((user) => user.userId === newUserId)) {
+      activeUsers.push({ userId: newUserId, socketId: socket.id });
+      console.log("New User Connected", activeUsers);
+    }
+    // send all active users to new user
+    io.emit("get-users", activeUsers);
+  });
+
+  socket.on("disconnect", () => {
+    // remove user from active users
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    console.log("User Disconnected", activeUsers);
+    // send all active users to all users
+    io.emit("get-users", activeUsers);
+  });
+
+  socket.on("offline", () => {
+    // remove user from active users
+    activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+    console.log("User Disconnected", activeUsers);
+    // send all active users to all users
+    io.emit("get-users", activeUsers);
+  });
+
+  // send message to a specific user
+  socket.on("send-message", (data) => {
+    const { receiverId } = data;
+    const user = activeUsers.find((user) => user.userId === receiverId);
+    console.log("Sending from socket to :", receiverId)
+    console.log("Data: ", data)
+    if (user) {
+      io.to(user.socketId).emit("recieve-message", data);
+    }
+  });
+});
+
+
+// routes
+import AuthRoute from './routes/auth.route.js'
+import UserRoute from './routes/user.route.js'
+import PostRoute from './routes/post.route.js'
+import UploadRoute from './routes/upload.route.js'
+import ChatRoute from './routes/chat.route.js'
+import MessageRoute from './routes/message.route.js'
+
+const app = express();
+
+
+// middleware
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+// to serve images inside public folder
+app.use(express.static('public')); 
+app.use('/images', express.static('images'));
+
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+const CONNECTION =process.env.MONGODB_CONNECTION;
+mongoose
+  .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+
+app.use('/auth', AuthRoute);
+app.use('/user', UserRoute)
+app.use('/posts', PostRoute)
+app.use('/upload', UploadRoute)
+app.use('/chat', ChatRoute)
+app.use('/message', MessageRoute)
